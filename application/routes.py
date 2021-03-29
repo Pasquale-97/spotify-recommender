554497e3104a598aed1315d57b4f65dbcd66305f
pasqualeiuliano <pasqualeiuliano97@gmail.com>
2021-03-29 13:51:11 +0100

    update to model

--- application/routes.py
+++ application/routes.py
@@ -1,7 +1,23 @@
 from application import app
+from flask import request
+from flask import jsonify
+from flask import render_template, redirect, url_for
+import pandas as pd
 
+from model import recommend_songs
 
-@app.route("/")
-def index():
-    return 'flask app is running'
+spotify_df = pd.read_csv("/Users/pasqualeiuliano/Documents/AI/spotify/data/data.csv")
 
+@app.route('/', methods=['GET', 'POST'])
+def main():
+    if request.method == 'GET':
+        return(render_template('index.html'))
+
+    if request.method == 'POST':
+        song = request.form['song']
+        
+        # x = 'Cupid\'s Chokehold / Breakfast in America - Radio Mix'
+        prediction = recommend_songs([{'name': song, 'year': 2007}],  spotify_df)
+
+    return render_template('index.html',
+                                result=prediction)
---
+++ model.py
@@ -0,0 +1,127 @@
+import os
+import numpy as np
+import pandas as pd
+
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+from sklearn.manifold import TSNE
+from sklearn.decomposition import PCA
+from sklearn.metrics import euclidean_distances
+from scipy.spatial.distance import cdist
+from yellowbrick.target import FeatureCorrelation
+
+from sklearn.decomposition import PCA
+from mpl_toolkits.mplot3d import Axes3D
+
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+from collections import defaultdict
+
+from collections import defaultdict
+import difflib
+import joblib
+
+import warnings
+warnings.filterwarnings("ignore")
+
+spotify_df = pd.read_csv("/Users/pasqualeiuliano/Documents/AI/spotify/data/data.csv")
+
+SPOTIPY_CLIENT_ID = '4ebf36973acc457ead8acce221e93db7'
+SPOTIPY_CLIENT_SECRET = '015d03c8de45471f8be99469d3b064c9'
+
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id='4ebf36973acc457ead8acce221e93db7',
+                                                           client_secret='015d03c8de45471f8be99469d3b064c9'))
+
+def find_song(name, artists):
+    song_data = defaultdict()
+    results = sp.search(q= 'track: {} artists: {}'.format(name, artists), limit=1)
+    
+    if results['tracks']['items'] == []:
+        return None
+    
+    results = results['tracks']['items'][0]
+    track_id = results['id']
+    audio_features = sp.audio_features(track_id)[0]
+
+    song_data['name'] = [name]
+    song_data['artists'] = [artists]
+    
+    song_data['explicit'] = [int(results['explicit'])]
+    song_data['duration_ms'] = [results['duration_ms']]
+    song_data['popularity'] = [results['popularity']]
+
+    for key, value in audio_features.items():
+        song_data[key] = value
+    
+    return pd.DataFrame(song_data)
+
+
+number_cols = ['valence', 'year', 'acousticness', 'danceability', 'duration_ms', 'energy', 'explicit',
+ 'instrumentalness', 'key', 'liveness', 'loudness', 'mode', 'popularity', 'speechiness', 'tempo', 'year']
+
+def get_song_data(song, spotify_data):
+    
+    try:
+        song_data = spotify_data[(spotify_data['name'] == song['name']) & (spotify_data['year'] == song['year'])].iloc[0]
+        return song_data
+    
+    except IndexError:
+        return find_song(song['name'], song['year'])
+        
+
+def get_mean_vector(song_list, spotify_data):
+    
+    song_vectors = []
+    
+    for song in song_list:
+        song_data = get_song_data(song, spotify_data)
+        if song_data is None:
+            print('The song: {} does not exist in Spotify or in database'.format(song['name']))
+            continue
+        song_vector = song_data[number_cols].values
+        song_vectors.append(song_vector)  
+    
+    song_matrix = np.array(list(song_vectors))
+    return np.mean(song_matrix, axis=0)
+
+
+def flatten_dict_list(dict_list):
+    
+    flattened_dict = defaultdict()
+
+    for key in dict_list[0].keys():
+        flattened_dict[key] = []
+    
+    for dictionary in dict_list:
+        for key, value in dictionary.items():
+            flattened_dict[key].append(value)
+            
+
+    return flattened_dict
+
+
+def recommend_songs( song_list, spotify_data, n_songs=100):
+
+    model = joblib.load('spotify-model.pkl')
+    
+    metadata_cols = ['name', 'year', 'artists', 'popularity']
+    song_dict = flatten_dict_list(song_list)
+    
+    song_center = get_mean_vector(song_list, spotify_data)
+    scaler = model.steps[0][1]
+
+    scaled_data = scaler.transform(spotify_data[number_cols])
+    scaled_song_center = scaler.transform(song_center.reshape(1, -1))
+    distances = cdist(scaled_song_center, scaled_data, 'cosine')
+    index = list(np.argsort(distances)[:, :n_songs][0])
+    
+    rec_songs = spotify_data.iloc[index]
+    rec_songs = rec_songs[~rec_songs['name'].isin(song_dict['name'])]
+    return rec_songs[metadata_cols].to_dict(orient='records')
+
+
+x = 'Cupid\'s Chokehold / Breakfast in America - Radio Mix'
+recommend_songs([
+                {'name': x, 'year': 2007}
+],  spotify_df)
