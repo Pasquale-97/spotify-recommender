2e7dd62be7f3ebb934122b9e2c7653b0d79d79dc
pasqualeiuliano <pasqualeiuliano97@gmail.com>
2021-03-31 00:07:35 +0100

    update to ui

--- application/routes.py
+++ application/routes.py
@@ -1,8 +1,10 @@
 from application import app
+from flask import Flask
 from flask import request
 from flask import jsonify
 from flask import render_template, redirect, url_for
 import pandas as pd
+import sys
 
 from model import recommend_songs
 
@@ -16,8 +18,14 @@ def main():
     if request.method == 'POST':
         song = request.form['song']
         
+        input_variables = pd.DataFrame([song])
+        
         # x = 'Cupid\'s Chokehold / Breakfast in America - Radio Mix'
-        prediction = recommend_songs([{'name': song, 'year': 2007}],  spotify_df)
+        prediction = recommend_songs([{'name': input_variables, 'year': 2007}],  spotify_df)
 
-    return render_template('index.html',
-                                result=prediction)
+        return render_template('index.html',
+                            original_input={'Song':song},
+                            result=prediction)
+
+if __name__ == '__main__':
+    app.run()
